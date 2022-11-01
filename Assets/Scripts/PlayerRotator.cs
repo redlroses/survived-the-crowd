9dcba2631bec940467f4f16fc486b8c43118dfb5
Evgeniy Batenkov <redlroses.rlr@gmail.com>
2022-11-01 23:54:32 +0500

    Added full steering control

--- Assets/Scripts/PlayerRotator.cs
+++ Assets/Scripts/PlayerRotator.cs
@@ -1,3 +1,4 @@
+using System;
 using Tools;
 using Tools.Extensions;
 using UnityEngine;
@@ -8,25 +9,32 @@ public sealed class PlayerRotator : MonoBehaviour
     [SerializeField] private Rigidbody _rigidbody;
     [SerializeField] private Car _vehicle;
     [SerializeField] private PlayerMover _mover;
-    [SerializeField] private float _rotationSpeed;
+    [SerializeField] private float _rotationSpeed = 1;
 
     private Quaternion _rotation;
+
+    private void FixedUpdate()
+    {
+        Quaternion targetRotation = Quaternion.LookRotation(_vehicle.Rudder.MoveDirection);
+        _rigidbody.rotation =
+            Quaternion.Lerp(_rigidbody.rotation, targetRotation, Time.fixedDeltaTime * _rotationSpeed);
+    }
 
     public void SetRotation(Vector2 direction)
     {
         direction = direction.RotateVector2(Constants.HalfPiDegrees);
-        float inputAngle = Mathf.Atan2(-direction.x, -direction.y) * Mathf.Rad2Deg;
+        float dotProduct = Vector2.Dot(direction, _mover.Direction);
         float moveAngle = Mathf.Atan2(_mover.Direction.x, _mover.Direction.y) * Mathf.Rad2Deg;
-        float angleDeviation = inputAngle - moveAngle;
-        angleDeviation = ClampAngle(angleDeviation);
-        Debug.Log($"inputAngle: {inputAngle}, moveAngle: {moveAngle}, angleDeviation: {angleDeviation}");
+        float inputAngle = CalculateInputAngle(direction, dotProduct);
+        float angleDeviation = moveAngle - inputAngle;
+        angleDeviation = ClampAngle(angleDeviation) * Math.Sign(dotProduct);
         _vehicle.Rudder.DeflectSteeringWheel(angleDeviation);
     }
 
-    private void RotateLegacy()
+    private static float CalculateInputAngle(Vector2 direction, float dotProduct)
     {
-        float rotationSpeed = Mathf.Clamp01(_mover.CurrentSpeed) * Time.fixedDeltaTime * _rotationSpeed;
-        _rigidbody.rotation = Quaternion.Lerp(_rigidbody.rotation, _rotation, rotationSpeed);
+        float dotSign = Mathf.Sign(dotProduct);
+        return Mathf.Atan2(direction.x * dotSign, direction.y * dotSign) * Mathf.Rad2Deg;
     }
 
     private float ClampAngle(float angle)
