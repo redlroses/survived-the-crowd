e61cd15fec4bcc6466626396c8cb474bddb692e6
Evgeniy Batenkov <redlroses.rlr@gmail.com>
2022-11-05 16:39:02 +0500

    Megre

--- Assets/Sources/test1.cs
+++ Assets/Sources/test1.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class test1 : MonoBehaviour
 {
     private int a = 5;
+    private int b = 5;
     
     // Start is called before the first frame update
     void Start()
