2cd50d5f56f0d3bda9e77555371ae2c9cc956047
Evgeniy Batenkov <redlroses.rlr@gmail.com>
2022-12-10 18:55:57 +0500

    Fixed enemy pool init bug

--- Assets/Sources/Pool/EnemyPool.cs
+++ Assets/Sources/Pool/EnemyPool.cs
@@ -4,16 +4,27 @@ namespace Sources.Pool
 {
     public sealed class EnemyPool : ObjectPool<Enemy.Enemy>
     {
+        private IAttackable _attackable;
+
         public void Init(IAttackable attackable)
         {
+            _attackable = attackable;
+
             FillPool();
 
             foreach (Enemy.Enemy copy in GetReadOnlyCopies())
             {
-                copy.GetComponent<AgentToTargetMover>().ApplyTarget(attackable);
-                copy.GetComponent<AgentPatternSwitcher>().Init(attackable);
-                copy.GetComponent<AgentAttackRangeTracker>().Init(attackable);
+                copy.GetComponent<AgentToTargetMover>().ApplyTarget(_attackable);
+                copy.GetComponent<AgentPatternSwitcher>().Init(_attackable);
+                copy.GetComponent<AgentAttackRangeTracker>().Init(_attackable);
             }
+        }
+
+        protected override void InitCopy(Enemy.Enemy copy)
+        {
+            copy.GetComponent<AgentToTargetMover>().ApplyTarget(_attackable);
+            copy.GetComponent<AgentPatternSwitcher>().Init(_attackable);
+            copy.GetComponent<AgentAttackRangeTracker>().Init(_attackable);
         }
     }
 }
